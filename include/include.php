7ff42a511c7e8d790668ac7711c18c36822a6a5c
Edwin <edds718@gmail.com>
2012-01-11 10:49:19 -0500

    v2

--- include/include.php
+++ include/include.php
@@ -20,11 +20,10 @@ session_start();
 //Global constants
 $APP_NAME = "PHP Couchbase WebChat";
 define('COUCHBASE_SERVER', "localhost");
-define('MEMBASE_PORT', 11211);
-define('COUCHBASE_PORT',5984);
-define('COUCHBASE_ADMIN_PORT',8091);
 define('APP_PREFIX',"chat");
 define('KEY_DELIM',"::");
+define('MEMBASE_SERVER',"localhost");
+define('MEMBASE_PORT',11211);
 
 #$csi = CouchbaseSingleton::getInstance();
 #$csi->getCouchbase()->flush();
@@ -50,7 +49,8 @@ class CouchbaseSingleton {
 	 */
 	private function initialize() {
 		self::$cb_obj = new Couchbase();
-		self::$cb_obj->addCouchbaseServer(COUCHBASE_SERVER,MEMBASE_PORT,COUCHBASE_PORT,COUCHBASE_SERVER,COUCHBASE_ADMIN_PORT);
+		self::$cb_obj->setOption(Memcached::OPT_COMPRESSION,false);
+        	self::$cb_obj->addServer(MEMBASE_SERVER, MEMBASE_PORT);
 	}
 
 	/**
@@ -101,20 +101,30 @@ class User {
 		}
 
 		$cb_obj = CouchbaseSingleton::getInstance() -> getCouchbase();
-		$view = $cb_obj->getView("chats","get_users");
-		$result = $view->getResultByKey($userid);
-		if(count($result->rows) == 0){
-	      $user_doc = new stdClass;
-         $user_doc->type = "user";
-         $user_doc->userid = $userid;
-      	$user_doc->sha1password = sha1($password);
-      	$json_doc = json_encode($user_doc);
-      	$res = $cb_obj->couchdb->saveDoc($json_doc);
+		//check to see if the userid already exists
+		$userid_key = APP_PREFIX . KEY_DELIM . "user" .
+KEY_DELIM . $userid;
+		$passwordHash = sha1($password);
+		if($cb_obj -> add($userid_key, $passwordHash)) {
+		//now that we've added the userid key we'll add it to the userlist
+			$userlist_key = APP_PREFIX . KEY_DELIM . "userlist";
+			if(!$cb_obj -> add($userlist_key, $userid)) {
+				$cb_obj -> append($userlist_key, KEY_DELIM . $userid);
+			}
+			return true;
 		} else {
-			$this -> last_error_string = "User id '<i>$userid</i>' exists, please choose another.";
+			$result_code = $cb_obj -> getResultCode();
+			if($result_code == Memcached::RES_NOTSTORED) {
+				$this -> last_error_string .=
+				"User id '<i>$userid</i>' exists, please choose another.";
+			} else {
+				$this -> last_error_string .=
+				"Error, please contact administrator:" .
+				$cb_obj -> getResultMessage();
+			}
 			return false;
 		}
-		return(true);
+
 	}
 
 	/**
@@ -124,17 +134,22 @@ class User {
 	 * @return boolean
 	 */
 	public function loginUser($userid, $password) {
+		$cb_obj = CouchbaseSingleton::getInstance() -> getCouchbase();
 		//check to sees if userid exists with the same password hashed
-      $cb_obj = CouchbaseSingleton::getInstance() -> getCouchbase();
-      $view = $cb_obj->getView("chats","get_users");
-      $result = $view->getResultByKey($userid);
-		if(count($result->rows)==1){
-			if($result->rows[0]->value->sha1password == sha1($password)){
-				$_SESSION{"userid"} = $userid;
-				return(true);
-			}
+		$userid_key = APP_PREFIX . KEY_DELIM .
+ "user" . KEY_DELIM . $userid;
+		$submitted_passwordHash = sha1($password);
+		$db_passwordHash = $cb_obj -> get($userid_key);
+		if($db_passwordHash == false) {
+			return (false);
 		}
-		return(false);
+		//do we match the password?
+		if($db_passwordHash == $submitted_passwordHash) {
+			$_SESSION{"userid"} = $userid;
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	/**
@@ -160,90 +175,55 @@ class User {
  * through the Memcached library
  */
 class Comments {
-	/**
-	 * Return the last n comments
-	 * @param integer $count
-	 * @return Array
-	 */
-	public function getLastComments($count) {
-		$cb_obj = CouchbaseSingleton::getInstance() -> getCouchbase();
-		#get the actual total number of comments, including deleted ones
-      $comment_count_key = APP_PREFIX . KEY_DELIM . "chatcount";
-      $comment_count = $cb_obj -> get($comment_count_key);
-		$view = $cb_obj->getView("chats","get_chats");
-		#use the Paginator to retrieve only (at most) $count comments
-		#from the most recent (highest comment number) counting down
-		$result_pages = $view->getResultPaginator();
-		$result_pages->setRowsPerPage($count);
-      $result_pages->setOptions(array("descending" => true));
-		$first_page = $result_pages->current();
-
-		#populate an associative array with keys numbered from the max
-		#comment number, counting down by $count
-		$comment_array = array();
-		for($i=$comment_count;$i>0 && $i>$comment_count-$count;$i--){
-			$comment_key = APP_PREFIX . KEY_DELIM . "COMMENT#" . $i;
-			$comment_array[$comment_key]=null;
-		}
-		#now fill the associative arraywith the keys that haven't been deleted
-		for($i=0;$i<count($first_page->rows) && $i<$count ;$i++){
-			$this_record = $first_page->rows[$i];
-			$comment_key = APP_PREFIX . KEY_DELIM . "COMMENT#" . $this_record->key;
-			if(array_key_exists($comment_key,$comment_array)){
-				$comment_value = $this_record->value->userid . KEY_DELIM . $this_record->value->chat;
-				$comment_array[$comment_key] = $comment_value;
-			}
-		}
-		return $comment_array;
-	}
-
-	/**
-	 * Add a comment to the comment list
-	 * @param string
-	 */
-	public function addComment($comment) {
-		if($comment != "") {
-      	$cb_obj = CouchbaseSingleton::getInstance() -> getCouchbase();
-			#get and increment the comment count index via the Membase
-			#API inherited by couchbase
+     /**
+      * Return the last n comments
+      * @param integer $count
+      * @return Array
+      */
+     public function getLastComments($count) {
+         $cb_obj = CouchbaseSingleton::getInstance() -> getCouchbase();
          $comment_count_key = APP_PREFIX . KEY_DELIM . "chatcount";
-         $comment_count = $cb_obj -> get($comment_count_key);
-         if($cb_obj -> getResultCode() == Memcached::RES_NOTFOUND) {
-            $cb_obj -> add($comment_count_key, 0);
-			}
-         $script_comment_count = $cb_obj -> increment($comment_count_key);
-			#create the chat object to save in the Coucbase record
-         $chat_doc = new stdClass;
-         $chat_doc->type = "chat";
-			$chat_doc->commentnum = $script_comment_count;
-         $chat_doc->userid = $_SESSION{"userid"};
-			$chat_doc->chat = $comment;
-			#encode and save the chat object
-         $json_doc = json_encode($chat_doc);
-         $res = $cb_obj->couchdb->saveDoc($json_doc);
-		}
-	}
-
-	/**
-	 * delete a comment from the list by number
-	 * @param integer $number
-	 */
-	public function deleteComment($number) {
-      $cb_obj = CouchbaseSingleton::getInstance() -> getCouchbase();
-		#retrieve the comment number to be deleted
-		$view = $cb_obj->getView("chats","get_chats");
-      $result = $view->getResultByKey(intval($number));
-      if(count($result->rows)==1){
-			#use the Membase api inherited by the Couchbase API to 
-			#delete the underlying Membase record by the id
-         $comment_count = $cb_obj -> delete($result->rows[0]->id);
-			#deletion is not synchronous so we give it a chance
-			#to catch up here by waiting for one second as we 
-			#will immediately be retrieving comments to show the list
-			sleep(1);
-			return(true);
-      }
-      return(false);
-	}
+         $script_comment_count = $cb_obj -> get($comment_count_key);
+         $comment_key_list = array();
+         for($i = $script_comment_count; $i > 0 &&
+              $i > ($script_comment_count - $count); $i--) {
+              array_push($comment_key_list, "COMMENT#" . $i);
+         }
+         $null = null;
+         return ($cb_obj -> getMulti($comment_key_list,
+             $null, Memcached::GET_PRESERVE_ORDER));
+     }
+     /**
+      * Add a comment to the comment list
+     * @param string
+     */
+    public function addComment($comment) {
+         $cb_obj = CouchbaseSingleton::getInstance() -> getCouchbase();
+         if($comment != "") {
+             $comment_count_key = APP_PREFIX . KEY_DELIM . "chatcount";
+             $comment_count = $cb_obj -> get($comment_count_key);
+             if($cb_obj -> getResultCode() == Memcached::RES_NOTFOUND) {
+                 $cb_obj -> add($comment_count_key, 0);
+             }
+             $script_comment_count = $cb_obj -> increment($comment_count_key);
+             $cb_obj -> add("COMMENT#" . $script_comment_count, $_SESSION{"userid"} .
+              KEY_DELIM . $_POST["comment"]);
+         }
+     }
+     /**
+      * delete a comment from the list by number
+      * @param integer $number
+      */
+     public function deleteComment($number) {
+         $cb_obj = CouchbaseSingleton::getInstance() -> getCouchbase();
+         $comment_key = "COMMENT#" . $number;
+         $result = $cb_obj -> get($comment_key);
+         $elements = explode(KEY_DELIM, $result);
+         $userid = array_shift($elements);
+         //make sure user who created is deleting
+         if($userid == $_SESSION{"userid"}) {
+             $result = $cb_obj -> delete($comment_key);
+         }
+     }
 }
 ?>
